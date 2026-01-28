c72f42818fa73dc908e2bde6e657ee40683961c1
MD. SHIPON <sshimi910@gmail.com>
2026-01-28 12:08:37 +0600

    update auth controller

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -51,9 +51,8 @@ export class AuthController {
 
     res.cookie('access_token', accessToken, {
       httpOnly: true,
-      // secure: process.env.NODE_ENV === 'production',
-      secure:true,
-      sameSite: 'none',
+      secure: process.env.NODE_ENV === 'production',   // false locally, true on prod
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000,
       path: '/',
     });
